efd6d7d18745b60b3eb932a25eec66746fa6f586
Peter Heesterman <heesterman.peter@gmail.com>
2019-03-21 15:48:31 +1300

    feat(Makesmall): simple first release

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Makesmall
+
+A tool that takes all the jpeg files in a folder and resizes to fit a 1920x1920 box
+
+(made for katja to use for her wordpress blog)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const log = console.log.bind(this)
+const fs = require('fs')
+const path = require('path')
+const R = require('ramda')
+const jimp = require('jimp');
+
+
+fs.readdir(process.cwd(), (err, files) => {
+  if (err) throw err
+  console.log(process.cwd())
+
+  const isJPEG = (fileName) => {
+    const extension = path.extname(fileName).toLowerCase()
+    return R.contains(extension, ['.jpg', '.jpeg'])
+  }
+  
+  const jpegs = R.filter(isJPEG, files)
+
+  console.log('Tranforming', jpegs.length === 0 ? 'nothing' : 'the following: \n')
+
+  let count = 0
+  jpegs.forEach(fileName => {
+    jimp.read(fileName, (err, image) => {
+      if (err) throw err
+      console.log('processing...', fileName)
+      image
+        .scaleToFit(1920, 1920)
+        .quality(35) // set JPEG quality -
+        .write(fileName)
+      count++
+      if (count === jpegs.length) {
+        console.log('\nDONE!')
+      }
+    })
+  })
+})
